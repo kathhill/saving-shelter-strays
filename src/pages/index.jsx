dea92ecd8c711ca9d1e13742e2eaeed1d0aa94a3
Kathy Hill <kath.hill@sbcglobal.net>
2022-11-28 13:15:38 -0800

    fix: Correct case

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import siteLogo from "../images/saving-shelter-strays-logo.png"
 import givingTuesdayLogoColor from "../images/giving-tuesday-logo-color-stacked.png"
